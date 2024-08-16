407c26fd54f9d8fe4118295bfc6ab9ff6b5d3f03
Raphael Braga Ribeiro <93213732+RaphaelBRibeiro@users.noreply.github.com>
2024-08-16 20:44:34 -0300

    Add files via upload

--- Exercicios/QUESTAO 02 - Universidade.sql
+++ Exercicios/QUESTAO 02 - Universidade.sql
@@ -40,8 +40,8 @@ CREATE TABLE Historico (
     Cod_Turma INT,
     Cod_Prof INT,
     Ano INT,
-    Frequencia INT,
-    Nota INT,
+    Frequencia varchar(4),
+    Nota float (4,2),
     PRIMARY KEY (MAT, Cod_Disc, Cod_Turma),
     FOREIGN KEY (MAT) REFERENCES Alunos (MAT),
     FOREIGN KEY (Cod_Disc) REFERENCES diciplina (Cod_Disc),
@@ -52,20 +52,23 @@ CREATE TABLE Historico (
 
 
 -- CRINANDO PROCEDURES
-Delimiter &
-create procedure Cadastro_Alunos (P_Mat int, P_nome varchar (30), P_Endereco varchar (30), P_Cidade (30))
-    begin 
-        insert into Alunos Values (P_Mat, P_Nome, P_Endereco, P_Cidade);
-    end 
+Delimiter & 
+create procedure Cadastro_Alunos(P_Mat int, P_nome varchar (30), P_Endereco varchar (30), P_cidade varchar (30))
+
+begin
+    insert into Alunos values (P_Mat, P_nome, P_Endereco, P_cidade);
+
+end 
 &
 Delimiter ;
 
 
 Delimiter &
 create procedure Cadastro_Diciplinas (P_Cod_Disc varchar (5), P_Nome_Disc varchar (30), P_Carga_Hora varchar (5))
-    begin 
-        insert into diciplina Values (P_Cod_Disc, P_Nome_Disc, P_Carga_Hora);
-    end 
+begin 
+    insert into diciplina Values (P_Cod_Disc, P_Nome_Disc, P_Carga_Hora);
+
+end 
 &
 Delimiter ;
 
@@ -87,6 +90,15 @@ end
 &
 Delimiter ;
 
+
+Delimiter & 
+create procedure Cadastro_Historico (P_MAT int, P_Cod_Disc Varchar (5), P_Cod_Turma int, P_Cod_Prof int, P_Ano int, P_Frequencia varchar(4), P_Nota float (4,2))
+Begin
+    insert into Historico values (P_MAT, P_Cod_Disc, P_Cod_Turma, P_Cod_Prof, P_Ano, P_Frequencia, P_Nota);
+end
+&
+Delimiter ;
+
 /*FALTA TABELA HISTORICO*/
 
 
@@ -95,16 +107,33 @@ call Cadastro_Alunos (2015010101, 'JOSE DE ALENCAR', 'RUA DAS ALMAS', 'NATAL');
 call Cadastro_Alunos (2015010102, 'JOÃO JOSÉ', 'AVENIDA RUY CARNEIRO', 'JOÃO PESSOA');
 call Cadastro_Alunos (2015010103, 'MARIA JOAQUINA', 'RUA CARROSSEL', 'RECIFE' );
 call Cadastro_Alunos (2015010104, 'MARIA DAS DORES', 'RUA DAS LADEIRAS', 'FORTALEZA');
-call Cadastro_Alunos (2015010105, 'JOSUÉ CLAUDINO DOS SANTOS', 'CENTRO', 'NATAL');
-call Cadastro_Alunos (2015010106, 'JOSUÉLISSON', 'CLAUDINO DOS SANTOS', 'CENTRO', 'NATAL');
+call Cadastro_Alunos (2015010105, 'JOSUÉ' 'CLAUDINO DOS SANTOS', 'CENTRO', 'NATAL');
+call Cadastro_Alunos (2015010106, 'JOSUÉLISSON CLAUDINO DOS SANTOS', 'CENTRO', 'NATAL');
 call Cadastro_Alunos (2015010107, 'ANA SOUZA', 'RUA DAS FLORES', 'SÃO PAULO');
 call Cadastro_Alunos (2015010108, 'CARLOS PEREIRA','AVENIDA BEIRA MAR', 'FORTALEZA');
 call Cadastro_Alunos (2015010109, 'FERNANDA SILVA', 'RUA DO SOL', 'RECIFE');
-call Cadastro_Alunos (2015010111, 'JOÃO COSTA', 'RUA DAS PALMEIRAS', 'CURITIBA');
+call Cadastro_Alunos (2015010110, 'JOÃO COSTA', 'RUA DAS PALMEIRAS', 'CURITIBA');
 
+insert into alunos Values (2015010106, 'JOSUÉLISSON', 'CLAUDINO DOS SANTOS', 'CENTRO');
+
+update alunos
+set nome = 'JOSUÉLISSON DOS SANTOS'
+where mat = 2015010106
+
+update alunos
+set endereco = 'Centro'
+where mat = 2015010106;
+
+update alunos
+set cidade = 'Fortaleza'
+where mat = 2015010106;
+
+update alunos 
+set Mat = 2015010110
+where mat = 2015010111
 
 call Cadastro_Diciplinas ('BD', 'BANCO DE DADOS', '100');
-call Cadastro_Diciplinas ('POO', 'PROGRAMAÇÃO COM ACESSO A BANCO DE DADOS', '100');
+call Cadastro_Diciplinas ('POO', 'PROG. ACESSO A BANCO DE DADOS', '100');
 call Cadastro_Diciplinas ('WEB', 'AUTORIA WEB', '50');
 call Cadastro_Diciplinas ('ENG', 'ENGENHARIA DE SOFTWARE', '80');
 
@@ -116,7 +145,63 @@ call Cadastro_Professor (192011, 'DIEGO OLIVEIRA',	'AVENIDA ROBERTO FREIRE',	'NA
 
 call Cadastro_Turma (1, 'BD', 212131, 2015, '11h_12h');
 call Cadastro_Turma (2, 'BD', 212131, 2015, '13h_14h');
-call Cadastro_Turma (1, 'POO', 192011, 2015, '08h-09h');
-call Cadastro_Turma (1, 'WEB', 192011, 2015, '07h_08h');
-call Cadastro_Turma (1, 'ENG', 122135, 2015, '10h_11h');
-call Cadastro_Turma (2, 'ENG', 122135, 2015, '09h_10h');
+call Cadastro_Turma (3, 'POO', 192011, 2015, '08h-09h');
+call Cadastro_Turma (4, 'WEB', 192011, 2015, '07h_08h');
+call Cadastro_Turma (5, 'ENG', 122135, 2015, '10h_11h');
+call Cadastro_Turma (6, 'ENG', 122135, 2015, '09h_10h');
+
+
+call Cadastro_Historico (2015010101, 'BD', 1, 212131, 2015,  '100%', 9.5);
+
+call Cadastro_Historico (2015010101, 'POO', 3, 192011, 2015, '85%', 10.0);
+
+call Cadastro_Historico (2015010102, 'BD', 2, 212131, 2015, '85%', 8.0);
+call Cadastro_Historico (2015010102, 'ENG', 6, 122135, 2015, '90%', 9.3);
+call Cadastro_Historico (2015010103, 'WEB', 4, 192011, 2015, '80%', 10.0);
+
+call Cadastro_Historico (2015010103, 'BD', 2, 212131, 2015, '90%', 8.5);
+call Cadastro_Historico (2015010104, 'BD', 1, 212131, 2015, '100%', 8.9);
+call Cadastro_Historico (2015010104, 'ENG', 5, 122135, 2015, '75%', 7.3);
+call Cadastro_Historico (2015010105, 'BD', 1, 212131, 2015, '95%', 9.0);
+call Cadastro_Historico (2015010105, 'POO', 3, 192011, 2015, '85%', 7.9);
+call Cadastro_Historico (2015010106, 'BD', 1, 212131, 2015, '85%', 8.1);
+call Cadastro_Historico (2015010106, 'WEB', 4, 192011, 2015, '100%', 10.0);
+
+call Cadastro_Historico (2015010107, 'POO', 3, 192011, 2015, '100%', 10.0);
+
+call Cadastro_Historico (2015010107, 'ENG', 6, 122135, 2015, '100%', 7.8);
+call Cadastro_Historico (2015010108, 'ENG', 6, 122135, 2015, '90%', 9.1);
+call Cadastro_Historico (2015010108, 'BD', 2, 212131, 2015, '85%', 7.0);
+call Cadastro_Historico (2015010109, 'BD', 2, 212131, 2015, '95%', 8.6);
+call Cadastro_Historico (2015010109, 'WEB', 4, 192011, 2015, '75%', 8.0);
+call Cadastro_Historico (2015010110, 'ENG', 5, 122135, 2015, '100%', 8.0);
+call Cadastro_Historico (2015010110, 'POO', 3, 192011, 2015, '95%', 7.4);
+
+update historico 
+set nota = 6.9
+where Mat = 2015010106 and Cod_Disc = 'BD';
+
+update historico
+set nota = 7
+where Mat = 2015010102 and Cod_Disc = 'BD'; 
+
+update historico
+set nota = 6.9
+where Mat = 2015010107 and Cod_Disc = 'POO'; 
+
+-- a) Encontre a MAT dos alunos com nota em BD em 2015 menor que 8.
+select H.Mat, H.nota, D.Cod_Disc
+from historico H
+    inner join diciplina D
+        on H.Cod_Disc = D.Cod_Disc
+where nota < 8 and D.Cod_Disc = 'BD' and ano = 2015;
+
+-- b) Encontre a MAT e calcule a média das notas dos alunos na disciplina de POO em 2015
+select TRUNCATE (AVG(nota),2 )as Media_Em_POO
+from historico
+where Cod_Disc = 'POO';
+-- c) Encontre a MAT e calcule a média das notas dos alunos na disciplina de POO em 2015 e que esta média seja superior a 6.
+select AVG(nota)as Media_Maior_6
+from historico
+where Cod_Disc = 'POO' and nota >
+-- d) Encontre quantos alunos não são de Natal.
